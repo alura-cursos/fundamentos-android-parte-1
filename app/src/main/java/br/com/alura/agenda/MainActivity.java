ec4f047886a5e39b06849067e2f3caed0636bbec
Alex Felipe <alexfelipevieira@gmail.com>
2018-11-13 10:37:58 -0200

    vídeo 1.5

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -1,6 +1,15 @@
 package br.com.alura.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Alex Felipe", Toast.LENGTH_LONG).show();
+    }
 }
